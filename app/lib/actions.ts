3759a0a6d8dd1b9722d5f6906cdbe05891389479
Autumn Staszeski <dev@autumn.ninja>
2025-01-16 16:34:36 -0500

    Add error handling
    learned about using not-found.tsx and error.tsx to handle errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,10 +25,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT into invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT into invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        return { message: "Failed to create invoice." };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -43,19 +47,27 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-        UPDATE invoices
-        SET customer_id = (${customerId}), amount = (${amountInCents}), status = (${status})
-        WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = (${customerId}), amount = (${amountInCents}), status = (${status})
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: `Failed to update invoice with id ${id}.` };
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`
-        DELETE from invoices WHERE id = ${id}
-    `;
+    try {
+        await sql`
+            DELETE from invoices WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: `Failed to delete invoice with id ${id}.` };
+    }
     revalidatePath('/dashboard/invoices');
 }
