68465e834511d1d63934bc0b6843d30937fa1669
Autumn Staszeski <dev@autumn.ninja>
2025-01-18 11:22:28 -0500

    Add login capabilities
    learn how to use next-auth to authenticate users and create a login page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
@@ -101,4 +103,23 @@ export async function deleteInvoice(id: string) {
         return { message: `Failed to delete invoice with id ${id}.` };
     }
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid Credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
